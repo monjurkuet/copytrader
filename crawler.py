15f29653a76b1d06b713409b27e8c5f5b774015a
monjurkuet <muhammad.manjur@outlook.com>
2023-04-21 14:40:05 +0600

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -5,55 +5,54 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import mysql.connector
+import json
 
 #detect platform
 SYSTEM_OS=platform.system()
+LEADERBOARD_URL='https://www.binance.com/bapi/futures/v3/public/future/leaderboard/getLeaderboardRank'
 
 def tor_browser():
     options = uc.ChromeOptions() 
     options.add_argument(f'--proxy-server=socks5://127.0.0.1:9050')
-    #options.user_data_dir = "/home/chromeprofileforbots"  
+    caps = options.to_capabilities()
+    caps['goog:loggingPrefs'] = {'performance': 'ALL'} 
     if SYSTEM_OS=='Linux':
-        return uc.Chrome(user_data_dir="/home/copytraderscrapingprofile",browser_executable_path='/usr/bin/brave-browser',headless=False,version_main=111,options=options) 
+        return uc.Chrome(user_data_dir="/home/copytraderscrapingprofile",browser_executable_path='/usr/bin/brave-browser',headless=False,version_main=111,options=options,desired_capabilities=caps) 
     elif SYSTEM_OS=='Windows':
-        return uc.Chrome(user_data_dir="G:\\copytraderscrapingprofile",browser_executable_path='C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\brave.exe',headless=False,version_main=111,options=options)      
+        return uc.Chrome(user_data_dir="G:\\copytraderscrapingprofile",browser_executable_path='C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe',headless=False,version_main=111,options=options,use_subprocess=True,desired_capabilities=caps)      
 
-def insert_details(company,rating,category,phone,website,claim_status,latitude,longitude,gmaps_url):
-    connection = mysql.connector.connect(
-                                host='localhost', 
-                                #host='161.97.97.183',
-                                database='google_maps',
-                                user='root', 
-                                password='$C0NTaB0vps8765%%$#', 
-                                port=3306
-                                ,auth_plugin='caching_sha2_password')
-    cursor = connection.cursor()  
-    sql_insert_with_param = """INSERT IGNORE INTO gmaps_details
-                            (company,rating,category,phone,website,claim_status,latitude,longitude,gmaps_url) 
-                            VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s);"""
-    val = (company,rating,category,phone,website,claim_status,latitude,longitude,gmaps_url)
-    cursor.execute(sql_insert_with_param , val)
-    connection.commit() 
-    print(val)
+def extract_position_data(log,driver):
+    request_id = log["params"]["requestId"]
+    resp_url = log["params"]["response"]["url"]
+    response_body=driver.execute_cdp_cmd("Network.getResponseBody", {"requestId": request_id})
+    response_body=json.loads(response_body['body'])
+    return response_body
 
-def update_gmaps_links(gmaps_url):
-    connection = mysql.connector.connect(
-                                host='localhost', 
-                                #host='161.97.97.183',
-                                database='google_maps',
-                                user='root', 
-                                password='$C0NTaB0vps8765%%$#', 
-                                port=3306
-                                ,auth_plugin='caching_sha2_password')
-    cursor = connection.cursor()  
-    sql = "UPDATE gmaps_links SET processed=1 WHERE gmaps_url = %s"
-    val = (gmaps_url,)
-    cursor.execute(sql, val)
-    connection.commit() 
-    print(val)
+def clean_logs(logs,target_url):
+    cleaned_log=None
+    for log in logs:
+        try:
+            resp_url = log["params"]["response"]["url"]
+            if resp_url==target_url:
+                cleaned_log=log
+                return cleaned_log
+        except:
+            pass   
 
-
+def extract_leaderboard_data(log,driver):
+     request_id = log["params"]["requestId"]
+     resp_url = log["params"]["response"]["url"]
+     response_body=driver.execute_cdp_cmd("Network.getResponseBody", {"requestId": request_id})
+     response_body=json.loads(response_body['body'])
+     return response_body
 
 if __name__ == "__main__":
-    gmaps_urls=geturls()
-    extract_gmaps_details(gmaps_urls)       
+    driver=tor_browser()
+    driver.get('https://www.binance.com/en/futures-activity/leaderboard/futures')
+    logs_raw = driver.get_log("performance")     
+    logs = [json.loads(lr["message"])["message"] for lr in logs_raw]
+    logs=clean_logs(logs,LEADERBOARD_URL)
+    if logs is not None:
+        response_body=extract_leaderboard_data(logs,driver)
+        data=response_body['data']
+    

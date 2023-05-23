47c1a26eab3a960da64094d6690e9b875abf8b01
monjurkuet <muhammad.manjur@outlook.com>
2023-05-23 13:07:01 +0600

    ELT final

--- traderwagon_elt.py
+++ traderwagon_elt.py
@@ -3,6 +3,44 @@ from datetime import datetime
 from datetime import timedelta
 import pandas as pd
 from collections import defaultdict
+import pymysql
+import logging
+import sshtunnel
+from sshtunnel import SSHTunnelForwarder
+
+# myql ssh tunnel
+ssh_host = '161.97.97.183'
+ssh_username = 'root'
+ssh_password = '$C0NTaB0vps8765%%$#'
+database_username = 'root'
+database_password = '$C0NTaB0vps8765%%$#'
+database_name = 'exchangetrading'
+localhost = '127.0.0.1'
+
+def open_ssh_tunnel(verbose=False):
+    if verbose:
+        sshtunnel.DEFAULT_LOGLEVEL = logging.DEBUG
+    global tunnel
+    tunnel = SSHTunnelForwarder(
+        (ssh_host, 22),
+        ssh_username = ssh_username,
+        ssh_password = ssh_password,
+        remote_bind_address = ('127.0.0.1', 3306)
+    )
+    tunnel.start()
+
+def mysql_connect():
+    global connection
+    connection = pymysql.connect(
+        host='127.0.0.1',
+        user=database_username,
+        passwd=database_password,
+        db=database_name,
+        port=tunnel.local_bind_port
+    )
+
+open_ssh_tunnel()
+mysql_connect()
 
 client = MongoClient('mongodb://myUserAdmin:%24C0NTaB0vps8765%25%25%24%23@161.97.97.183:27017/?authMechanism=DEFAULT')
 db = client['exchanges']
@@ -33,3 +71,24 @@ for item in dd:
             final.append(item)
     except:
         pass
+
+for each_item in final:
+    portfolioId=each_item['portfolioId']
+    last7DRoi=each_item['performancedata']['last7DRoi']
+    last30DRoi=each_item['performancedata']['last30DRoi']
+    last90DRoi=each_item['performancedata']['last90DRoi']
+    for each_position in each_item['positiondata']:
+        symbol=each_position['symbol']
+        entryPrice=each_position['entryPrice']
+        positionAmount=each_position['positionAmount']
+        positionSide=each_position['positionSide']
+        cursor = connection.cursor() 
+        sql_insert_with_param = """REPLACE INTO traderwagon
+                            (portfolioId,symbol,entryPrice,positionAmount,positionSide,last7DRoi,last30DRoi,last90DRoi) 
+                            VALUES (%s,%s,%s,%s,%s,%s,%s,%s);""" 
+        data_tuple = (portfolioId,symbol,entryPrice,positionAmount,positionSide,last7DRoi,last30DRoi,last90DRoi)
+        cursor.execute(sql_insert_with_param, data_tuple)
+        connection.commit() 
+        print(data_tuple)
+        
+
